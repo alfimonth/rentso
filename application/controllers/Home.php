7b971ac8ea34e63e21e0f59558e0fc62596ad043
alfimonth <alvy1610@gmail.com>
2023-05-25 21:58:52 +0700

    menambahkan controller home

---
+++ application/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+class Home extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+    public function index()
+    {
+        
+        
+        
+    }
+
+}

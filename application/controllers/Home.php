b37995a7a741100a4a135af28c54d25d71ad1029
alfimonth <alvy1610@gmail.com>
2023-05-26 10:47:57 +0700

    update controller home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,9 +7,28 @@ class Home extends CI_Controller
     }
     public function index()
     {
-        
-        
-        
-    }
 
+        $this->load->view('front-end/index');
+    }
+    public function logout()
+    {
+
+        session_start();
+        $_SESSION = array();
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 60 * 60,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+        unset($_SESSION['ulogin']);
+        session_destroy(); // destroy session
+        redirect('');
+    }
 }

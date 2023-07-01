e846e4f2fe3d402d97244500c0e18a4f771653fd
alfimonth <alvy1610@gmail.com>
2023-07-02 06:26:38 +0700

    membuat halaman 404

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -1,64 +1,126 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
-<title>404 Page Not Found</title>
-<style type="text/css">
+	<meta charset="utf-8">
+	<title>404 Page Not Found</title>
+	<style type="text/css">
+		body {
+			background-color: #f1f1f1;
+			font-family: Arial, sans-serif;
+		}
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
+		.container {
+			width: 400px;
+			margin: 100px auto;
+			background-color: #fff;
+			border: 1px solid #ccc;
+			border-radius: 5px;
+			padding: 20px;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+			animation: shake 0.5s ease-in-out;
+		}
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
+		@keyframes shake {
+			0% {
+				transform: translateX(0);
+			}
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
+			10% {
+				transform: translateX(-10px);
+			}
 
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
+			20% {
+				transform: translateX(10px);
+			}
 
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
+			30% {
+				transform: translateX(-10px);
+			}
 
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
+			40% {
+				transform: translateX(10px);
+			}
 
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
+			50% {
+				transform: translateX(-10px);
+			}
+
+			60% {
+				transform: translateX(10px);
+			}
+
+			70% {
+				transform: translateX(-10px);
+			}
+
+			80% {
+				transform: translateX(10px);
+			}
+
+			90% {
+				transform: translateX(-10px);
+			}
+
+			100% {
+				transform: translateX(0);
+			}
+		}
+
+		h1 {
+			color: #555;
+			font-size: 24px;
+			margin-bottom: 10px;
+		}
+
+		p {
+			color: #777;
+			margin-bottom: 20px;
+		}
+
+		a {
+			color: #337ab7;
+			text-decoration: none;
+		}
+
+		a:hover {
+			text-decoration: underline;
+		}
+
+		.error-code {
+			font-size: 64px;
+			color: #337ab7;
+			margin-bottom: 20px;
+		}
+
+		.error-message {
+			font-size: 18px;
+			color: #777;
+			margin-bottom: 20px;
+		}
+
+		.back-link {
+			display: inline-block;
+			padding: 10px 20px;
+			background-color: #337ab7;
+			color: #fff;
+			border-radius: 3px;
+			text-decoration: none;
+		}
+
+		.back-link:hover {
+			background-color: #13537f;
+		}
+	</style>
 </head>
+
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
+	<div class="container">
+		<h1>404 Page Not Found</h1>
+		<p class="error-code">404</p>
+		<p class="error-message">Oops! The page you requested could not be found.</p>
+		<a href="http://localhost/rentso" class="back-link">Go Back</a>
 	</div>
 </body>
+
 </html>
